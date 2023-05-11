05a4d1a3f66b45a845444a4be89c615f12865c8f
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-05-11 11:49:49 +0500

    added Search box

---
+++ src/components/search-box/SearchBox.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./search-box.css";
+export default class SearchBox extends Component {
+	render() {
+		return (
+			<div>
+				<input
+					className={`search-box ${this.props.className}`}
+					type="search"
+					placeholder={this.props.placeholder}
+					onChange={this.props.onChangeHandler}
+				/>
+			</div>
+		);
+	}
+}
